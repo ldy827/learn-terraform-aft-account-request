bbc2f5b822df5fc27026e3947f753a75c38395b9
ldy827 <103937790+ldy827@users.noreply.github.com>
2023-01-04 11:37:43 +0900

    Create ndnedev.tf

---
+++ terraform/ndnedev.tf
@@ -0,0 +1,28 @@
+module "ndne-dev" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "awstest0104.ndne.dev@gmail.com"
+    AccountName               = "ndne-dev"
+    ManagedOrganizationalUnit = "DEV (ou-ng9l-7x3kzsme)"
+    SSOUserEmail              = "awstest0104.ndne.dev@gmail.com"
+    SSOUserFirstName          = "DAE YOUNG"
+    SSOUserLastName           = "LEE"
+  }
+
+  account_tags = {
+    "Project" = "NDNE"
+    "Environment" = "DEV"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "CoE OU Operator"
+    change_reason       = "Add Account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "ndnedev"
+}
